40c4e0f5f6035ba2c91fa5d67c9c054ff61ecba7
hyperelastic <ador.elgrozni@gmail.com>
2017-03-21 00:55:12 +0000

    Basic manager worker with ncurses.

--- main.c
+++ main.c
@@ -9,41 +9,65 @@
 #define NUMTHRDS 2
 
 pthread_t threads [NUMTHRDS];
+int manager_state = 0;
 int i;
 
 static void *worker(void *_){
-    /* once this is hit, block all new threads untill unlock */
-    int j;
-    for (j=0; j<5; ++j) {
-        printf("\tj: %d\n", j);
-        usleep(1e5);
-    }
+    mvprintw(5, 5, "\ti: %d\n", i);
+    refresh();
+    pthread_exit(NULL);
 }
-static void *manager(void *_){
+
+static void *manager(void *arg){
+    int* pmanager_state = arg;
     while (1) {
-        printf("i: %d", i);
-        pthread_create(&threads[1], NULL, worker, NULL);
-        usleep(1e6);
-        pthread_join(threads[1], NULL);
-        i++;
+        switch ( *pmanager_state ) {
+            case 0: { // normal
+                pthread_create(&threads[1], NULL, worker, NULL);
+                ++i;
+                usleep(1e6);
+                break;
+            }
+            case -1: { // clean
+                pthread_exit(NULL);
+                break;
+            }
+            case 1: { // special
+                usleep(1e6);
+                break;
+            }
+        }
     }
 }
 
 int main() {
-    pthread_create(&threads[0], NULL, manager, NULL);
+    int i = 0;
+    int key;
 
-    //must have this as main will block until all the supported threads are done
+    initscr();
+    keypad(stdscr, 1);
+    cbreak();
+    noecho();
+    refresh();
+    mvprintw(1, 1, "Press any key, enter for exit.");
+
+    pthread_create(&threads[0], NULL, manager, (void*)&manager_state);
+    while (1) {
+        key = getch();
+        if (key==10) {
+            wclear(stdscr);
+            mvprintw(10, 10, "Exiting");
+            refresh();
+            manager_state = -1;
+            break;
+        }
+        else {
+            mvprintw(10, 10, "Your input was %c\n", key);
+        }
+    }
+ 
     pthread_exit(NULL);
+    endwin();
 
-//    initscr();
-//    cbreak();
-//    noecho();
-//
-//    printw("asdf");
-//
-//    refresh();
-//    getch();
-//    endwin();
-//
     return 0;
 }
